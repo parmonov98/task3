fd0a3583660640356cf06c9fc2d7fbfcd3ff095b
Murod <parmonov98@yandex.ru>
2020-03-03 06:48:00 +0500

    putting the DB methods

--- all_class/database.php
+++ all_class/database.php
@@ -15,6 +15,25 @@ class database
 		return $this->db;
 	}
 
+	function query_result_array($query){
+
+		$result = mysqli_query($this->base,$query); // result of query
+		
+		if(mysqli_num_rows($result)){ // if this user has base 
+			$new_user_array=[];
+			while ($row = mysqli_fetch_array($result)) {
+				$new_user_array[]=$row;
+			}
+			return $new_user_array;
+		}
+		else{
+			return "bunday user yoq"; // bunday user yo`	
+		}
+	}
+	
+	function query_result($query){
+		mysqli_query($this->base,$query);
+	}
 
 
 }
